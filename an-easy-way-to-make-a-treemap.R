3a438d0eae6e59684753eaa8f8002e537e52b7cf
Bart Kastermans <kasterma@kasterma.net>
2017-01-19 06:47:13 +0100

    treemap

---
+++ an-easy-way-to-make-a-treemap.R
@@ -0,0 +1,4 @@
+library(portfolio)
+
+data <- read.csv("http://datasets.flowingdata.com/post-data.txt")
+map.market(id=data$id, area=data$views, group=data$category, color=data$comments, main="FlowingData Map")
